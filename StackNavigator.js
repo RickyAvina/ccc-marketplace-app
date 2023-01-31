a71fe1aa26130c5fd136dafe461e72646170c9cd
Enrique Avina <rickyavina@icloud.com>
2023-01-31 16:06:46 -0500

    Added Tailwind and react native navigation

---
+++ StackNavigator.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Group>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Group>
+    </Stack.Navigator>
+  )
+}
+
+export default StackNavigator
